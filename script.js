cfb846b6e5c61a75ed23c845a0b1cea5dba358a7
Guillermo Jimenez <W0484522@campus.nscc.ca>
2023-04-06 13:06:25 -0300

    html: rearranged lines under "Area In:" , linked wikibutton to google (for now) AND added lines by adding formRef variable for document.querySelector and selectedCountry as another new varibale for country selection

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getCountryInfo(menuRef)
 {
-
+    //alert("Hello Visitor!!"); 
+    //declare a reference to the form 
+    let formRef = document.querySelector("#countryInfoForm");
+    let selectedCountry = formRef["countryselection"].value;
+    alert(selectedCountry);
 }
