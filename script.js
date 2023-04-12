7c5c2bd979cae784a5dc50e03c44d2772a067cdc
Guillermo Jimenez <W0484522@campus.nscc.ca>
2023-04-12 01:11:43 -0300

    added an alert for selected country PLUS unit selected. In addition to reorganizing html script and add "checked" for miles and km

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
-function getCountryInfo(menuRef)
+alert("Greetings! And Welcome to World Encyclopedia!!");
+
+// read data from csv file
+function getCountryInfo()
 {
-    //alert("Hello Visitor!!"); 
+
     //declare a reference to the form 
     let formRef = document.querySelector("#countryInfoForm");
-    let selectedCountry = formRef["countryselection"].value;
+    let selectedCountry = formRef["countrySelection"].value.replace(" "," ");
     alert(selectedCountry);
+    let pUnitSelected = formRef["pUnitSelection"].value; 
+    alert(pUnitSelected);
+
+    //change the href of the wikipage link/button 
+    let wikiLink = document.querySelector("#wikiPageLink");
+    wikiLink.setAttribute("href", "https://en.wikipedia.org/wiki/"+selectedCountry);
 }
+
+//calculate population %
+
+//calculate population density 
+
+//convert sq miles km / vice versa 
+
+//convert per sq miles to per sq km / vice versa
